1d492253f679b07e68d261641ed08b73f64b394a
Jukka Michelsson <jukkamic@gmail.com>
2023-03-04 20:39:13 +0200

    debug

--- application/hello-flask.py
+++ application/hello-flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello! GitHub triggered build. Pipeline triggered deployment. V3."
+    return "Hello! GitHub triggered build. Pipeline triggered deployment. V4."
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=80)
