917719b8573d315b96b3259869c1448d3f8bfe47
Jukka Michelsson <jukkamic@gmail.com>
2023-03-04 18:24:03 +0200

    new version

--- application/hello-flask.py
+++ application/hello-flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask 4"
+    return "Hello, Flask. GitHub triggered build."
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=80)
