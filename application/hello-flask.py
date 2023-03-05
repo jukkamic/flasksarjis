a8600abc75c62351e6c6aab342832d89da28eded
Jukka Michelsson <jukkamic@gmail.com>
2023-03-05 03:42:55 +0200

    Updated welcome page

--- application/hello-flask.py
+++ application/hello-flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Sweet Jesus! GitHub triggered build. Pipeline triggered deployment. V5."
+    return "Sweet Jesus! GitHub triggered CodePipeline. CodePipeline triggered CodeBuild. CodeBuild built image to ECR. Pipeline sent image to ECS. LoadBalancer says HELLLLLO!!!"
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=80)
