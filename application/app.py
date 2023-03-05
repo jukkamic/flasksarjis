4adc8cd0e73f355c6188825786aad7e088ae36bf
Jukka Michelsson <jukkamic@gmail.com>
2023-03-06 01:55:14 +0200

    updated welcome page

--- application/app.py
+++ application/app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Sweet Jesus! GitHub triggered CodePipeline. CodePipeline triggered CodeBuild. CodeBuild built image to ECR. Pipeline sent image to ECS. LoadBalancer says HELLLLLO!!!"
+    return "Sweet Jesus! GitHub triggered CodePipeline. CodePipeline triggered CodeBuild. CodeBuild built image to ECR. Pipeline sent image to ECS. LoadBalancer says HELLLLLO!!! ****AND NOW WITH WSGI**** (gonna need a html template for this)"
 
