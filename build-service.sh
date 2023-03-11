80e6e68819130fde719c633b21eaca8e85dba422
Jukka Michelsson <jukkamic@gmail.com>
2023-03-11 03:07:52 +0200

    rename dockers

--- build-service.sh
+++ build-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t sarjis-service:latest ..
+docker build -t sarjis-service:latest -f service/Dockerfile .
 docker tag sarjis-service:latest 634129605042.dkr.ecr.eu-west-1.amazonaws.com/sarjis-service:latest
 
