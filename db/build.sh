943035e3369bca9e4e7589d31f9c054506775c38
Jukka Michelsson <jukkamic@gmail.com>
2023-03-11 03:18:25 +0200

    fix builds

--- db/build.sh
+++ db/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build -t sarjis-db:latest --build-arg postgres_password=secret .
+docker tag sarjis-db:latest 634129605042.dkr.ecr.eu-west-1.amazonaws.com/sarjis-db:latest
--- web/build.sh
+++ web/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker build -t sarjis-web:latest .
+docker tag sarjis-web:latest 634129605042.dkr.ecr.eu-west-1.amazonaws.com/sarjis-web:latest
+
